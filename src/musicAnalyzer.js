f1a7a99252d0f151fd3a2b665fb0c1e455317c81
zxc7410wer <mj@mjui-MacBook-Pro.local>
2017-08-06 15:48:25 +0900

    use webpack

---
+++ src/musicAnalyzer.js
@@ -0,0 +1,5 @@
+var module = require('./util/getDecodeData.js');
+
+// console.log('success');
+// module.getDecodeData();
+// getDecodeData();
--- src/util/getDecodeData.js
+++ src/util/getDecodeData.js
@@ -1,20 +1,62 @@
-export default function getDecodeData() {
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-var source;
-var temp = new Uint8Array( 1200 );
+// export default function getDecodeData() {
+// var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+// var source;
+// var temp = new Uint8Array( 1200 );
 
-function getData() {
+// function getData() {
+//   source = audioCtx.createBufferSource();
+//   var request = new XMLHttpRequest();
+
+//  request.open('GET', '../../audio/Odesza_Above_The_Middle.mp3', true);
+//   request.responseType = 'arraybuffer';
+
+
+//  request.onload = function() {
+//     var audioData = request.response;
+
+//    audioCtx.decodeAudioData(audioData, function(buffer) {
+//       source.buffer = buffer;
+//       console.log(buffer.length)
+//       console.log( buffer );
+//       var convert = new Uint8Array( buffer.getChannelData( 0 ).buffer );
+//       var convert2 = new Uint8Array( buffer.getChannelData( 1 ).buffer );
+//       console.log( convert );
+//       console.log( convert2 );
+//       temp = convert.subarray(0, 1200 );
+//       source.connect(audioCtx.destination);
+//       source.loop = true;
+//       return buffer;
+//    },
+//     function(e){ console.log("Error with decoding audio data" + e.err); });
+//  }
+//  request.send();
+// }
+// getData();
+// }
+
+// var getDecodeData = function() {
+//   console.log(1);
+  
+
+
+//   getData();
+// };
+
+var getDecodeData = function() {
+  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  var source;
+  var temp = new Uint8Array( 1200 );
+
   source = audioCtx.createBufferSource();
   var request = new XMLHttpRequest();
 
- request.open('GET', '../../audio/Odesza_Above_The_Middle.mp3', true);
+  request.open('GET', '../../audio/Odesza_Above_The_Middle.mp3', true);
   request.responseType = 'arraybuffer';
 
-
- request.onload = function() {
+  request.onload = function() {
     var audioData = request.response;
 
-   audioCtx.decodeAudioData(audioData, function(buffer) {
+    audioCtx.decodeAudioData(audioData, function(buffer) {
       source.buffer = buffer;
       console.log(buffer.length)
       console.log( buffer );
@@ -26,10 +68,12 @@ function getData() {
       source.connect(audioCtx.destination);
       source.loop = true;
       return buffer;
-   },
+    },
+
     function(e){ console.log("Error with decoding audio data" + e.err); });
- }
- request.send();
+  }
+
+  request.send();
 }
-getData();
-}
+
+module.exports = getDecodeData();
