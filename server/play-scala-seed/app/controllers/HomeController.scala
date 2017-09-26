fa61252c405a3db9ace85d06fbc47185d7e596dc
LinePlus <lineplus@malgogi-ipv6test.local>
2017-09-26 23:45:09 +0900

    update

--- server/play-scala-seed/app/controllers/HomeController.scala
+++ server/play-scala-seed/app/controllers/HomeController.scala
@@ -2,20 +2,25 @@ package controllers
 
 import javax.inject._
 
+import akka.actor.ActorSystem
+import play.api.libs.concurrent.CustomExecutionContext
 import play.api.libs.ws.{WSClient, WSRequest, WSResponse}
 import play.api.mvc._
 
-
-import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.duration._
 import utils.IDGenerator
+
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
+
 @Singleton
-class HomeController @Inject()(cc: ControllerComponents, ws: WSClient, idGenerator:IDGenerator ) extends AbstractController(cc) {
-  val MY_URL = "http://localhost:9090/test";
+class HomeController @Inject()( cc: ControllerComponents, ws: WSClient, idGenerator:IDGenerator ) extends AbstractController(cc) {
+  final val MY_URL = "http://localhost:9090";
+  final val MP3_DIRECTORY = play.Environment.simple().rootPath().getAbsolutePath() + "/tmp/mp3/"
 
   /**
    * Create an Action to render an HTML page.
@@ -28,24 +33,38 @@ class HomeController @Inject()(cc: ControllerComponents, ws: WSClient, idGenerat
     Ok(views.html.index())
   }
 
-  def upload() = Action { implicit request : Request[AnyContent] =>
+  def upload( ) = Action { implicit request : Request[AnyContent] =>
     request.body.asMultipartFormData.map{ data =>
       import java.io.File
 
       val filename = data.file("picture" ).get.filename
       val contentType = data.file("picture").get.contentType;
-      data.file("picture").get.ref.moveTo( new File( play.Environment.simple().rootPath() + "/tmp/picture/" + filename  ) );
-      Redirect( routes.HomeController.result( idGenerator.generate( filename ) ) );
 
+      data.file("picture").get.ref.moveTo( new File(  MP3_DIRECTORY + filename  ) );
 
+      Redirect( routes.HomeController.result( filename ) );
     }.getOrElse {
+
       Redirect( routes.HomeController.index() ).flashing(
-        "error" -> "Missing file")
+        "error" -> "Missing file");
     }
   }
 
-  def result( id:String ) = Action { implicit request : Request[AnyContent] =>
-    Ok( views.html.result( idGenerator.generate( id ) ) );
+
+  def result( id:String ) = Action.async { implicit request : Request[AnyContent] =>
+
+
+    val request: WSRequest = ws.url( MY_URL );
+    val complexRequest: WSRequest = request
+      .withHttpHeaders( "Content-Type" -> "application/json" )
+      .addQueryStringParameters( "path" -> MP3_DIRECTORY )
+      .addQueryStringParameters( "filename" -> id )
+      .withRequestTimeout(1000000.millis);
+
+    complexRequest.get().map( resp => {
+      print( ( resp.json \ "results" ).as[List[Double]].toString() );
+      Ok( views.html.result( idGenerator.generate( id ), ( resp.json \ "results" ).as[List[Double]] ) );
+    });
   }
 
   def login() = Action { implicit request : Request[AnyContent] =>
@@ -56,17 +75,11 @@ class HomeController @Inject()(cc: ControllerComponents, ws: WSClient, idGenerat
     }
   }
 
-  def testClient() = Action { implicit  request : Request[AnyContent] =>
 
-      val request: WSRequest = ws.url( MY_URL );
 
-      val complexRequest: WSRequest = request
-        .addQueryStringParameters("search" -> "play")
-        .withRequestTimeout(10000.millis);
+  def requestFile( fileName : String ) = {
 
-      val resp:Future[WSResponse] = complexRequest.get();
 
-      Ok( views.html.result( idGenerator.generate( "test234" ) ) );
   }
 
 }
